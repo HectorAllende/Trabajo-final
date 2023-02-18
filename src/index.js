f74731bb4b88d369f7c0fbd4a6df60589dbc9b2f
HectorAllende <54869462+HectorAllende@users.noreply.github.com>
2023-02-17 23:09:33 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import ProductManager from "./controllers/ProductManager.js"
+
+const product = new ProductManager()
+
+
+const app = express()
+const PORT = 4000
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+app.post("/products", async (req, res) =>{
+    let newProduct = req.body
+    res.send(await product.writeProducts(newProduct))
+})
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor Express Puerto ${PORT}`)
+})
