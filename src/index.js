95a81606256ec934a2ecd1f0c0e7c38c6748f788
HectorAllende <54869462+HectorAllende@users.noreply.github.com>
2023-03-02 00:12:31 -0300

    Agregando Handlebars

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
 import express from "express"
-
 import ProductRouter from "./router/product.routes.js"
-
 import CartRouter from "./router/carts.routes.js"
+import { engine } from "express-handlebars"
+import * as path from 'path'
+import ___dirname from "./utils.js"
+import ProductManager from "./controllers/ProductManager.js"
 
 const app = express()
 const PORT = 4000
+const product = new ProductManager()
 
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }))
+
+// Handlebars
+
+app.engine("handlebars", engine())
+app.set('view engine', "handlebars")
+app.set("views", path.resolve(___dirname + "/views"))
+
+// Static
+
+app.use("/", express.static(___dirname + "/public"))
+
+app.get("/", async (req, res) => {
+    let allProducts = await product.getProducts()
+    res.render("home", {
+        title: "Express Avanzado | Handlebars",
+        products: allProducts
+    })
+})
+
+app.get("/:id", async (req, res) => {
+    let prod = await product.getProductsById(req.params.id)
+    res.render("prod", {
+        title: "Express Avanzado | Handlebars",
+        products: prod
+    })
+})
 
 app.use('/api/products', ProductRouter)
 app.use('/api/cart', CartRouter)
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Servidor Express Puerto ${PORT}`)
 })
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+import path from "path"
+
+import { fileURLToPath } from "url"
+
+const ___filename = fileURLToPath (import.meta.url)
+const ___dirname = path.dirname(___filename)
+
+export default ___dirname
